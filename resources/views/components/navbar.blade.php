ac0914045ae99ef808a98067c9e4292ef8c50975
Vincenzo Rosa <vincenzo.rosa99@gmail.com>
2026-04-01 23:14:12 +0200

    selfwork laravel 10

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,56 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+
+        {{-- HOME --}}
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
+        </li>
+
+       
+
+
+
+
+      @guest
+      {{-- REGISTRATI --}}
+          <li class="nav-item">
+        <a class="nav-link" href="{{ route('register') }}">Registrati</a>
+      </li>
+
+      {{-- ACCEDI --}}
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('login') }}">Accedi</a>
+      </li>
+ 
+      @endguest
+     
+
+      @auth
+      {{-- MESSAGGIO BENVENUTO ALL'UTENTE --}}
+      <li class="nav-item">
+        <a class="nav-link" href="#">Benvenut* {{Auth::user()->name}}</a>
+      </li>
+
+      {{-- LOGOUT --}}
+         <li class="nav-item">
+        <form  action="{{ route('logout') }}" method="POST">
+          @csrf
+          <button class="nav-link" type="submit" >Logout</button>
+        </form>
+      </li> 
+
+
+      @endauth
+      
+      
+      
+    </ul>
+  </div>
+</div>
+</nav>
